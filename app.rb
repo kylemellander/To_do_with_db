128c1cc577b48756362a26e6a319a428bfb35145
Kyle Mellander & Rachael Berecka <student@epicodus.com>
2015-08-18 09:46:07 -0700

    Basic frontend for index page made

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
+require('./lib/list')
 require('./lib/task')
 require('pg')
 
 DB = PG.connect({:dbname => "to_do"})
+
+get('/') do
+  @lists = List.all()
+  erb(:index)
+end
